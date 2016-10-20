8df954bf3d0caca63246cdbd60505157bb471a79
Journey <timtag1190@gmail.com>
2016-10-20 12:54:43 -0500

    Update Newbie.js

--- Newbie.js
+++ Newbie.js
@@ -2,48 +2,39 @@
 // This is CODE, lets you control your character with code.
 // If you don't know how to code, don't worry, It's easy.
 // Just set attack_mode to true and ENGAGE!
-
 var attack_mode = true
 
-setInterval(function()
-{
-    if (character.hp < character.max_hp - 200 || character.mp < character.max_mp - 500)
-    {
-        use_hp_or_mp();
-    }
-    loot();
+setInterval(function() {
+	if (character.hp < character.max_hp - 200 || character.mp < character.max_mp - 500) {
+		use_hp_or_mp();
+	}
+	loot();
 
-    if (!attack_mode || character.moving) return;
+	if (!attack_mode || character.moving) return;
 
-    var target = get_targeted_monster();
-    if (!target)
-    {
-        target = get_nearest_monster(
-        {
-            min_xp: 100,
-            max_att: 40
-        });
-        if (target) change_target(target);
-        else
-        {
-            set_message("No Monsters");
-            return;
-        }
-    }
+	var target = get_targeted_monster();
+	if (!target) {
+		target = get_nearest_monster({
+			min_xp: 100,
+			max_att: 40
+		});
+		if (target) change_target(target);
+		else {
+			set_message("No Monsters");
+			return;
+		}
+	}
 
-    if (!in_attack_range(target))
-    {
-        move(
-            character.real_x + (target.real_x - character.real_x) / 2,
-            character.real_y + (target.real_y - character.real_y) / 2
-        );
-        // Walk half the distance
-    }
-    else if (can_attack(target))
-    {
-        set_message("Attacking");
-        attack(target);
-    }
+	if (!in_attack_range(target)) {
+		move(
+			character.real_x + (target.real_x - character.real_x) / 2,
+			character.real_y + (target.real_y - character.real_y) / 2
+		);
+		// Walk half the distance
+	} else if (can_attack(target)) {
+		set_message("Attacking");
+		attack(target);
+	}
 
 }, 1000 / 4); // Loops every 1/4 seconds.
 
