efee31362ff81135fdda3c133c641904be79b2de
JourneyOver <timtag1190@gmail.com>
2016-10-20 09:06:27 -0500

    Beautify code a bit and update basic grinding
    ~Beautify code
    ~update basic grinding to include option to standstill and attack.

--- Basic Grinding.js
+++ Basic Grinding.js
@@ -1,5 +1,6 @@
-/* var enemydist = parent.G.monsters.<targettype>.range + 5; //Change <targettype> to whatever you are fighting, list of names for each monster will be in targettype variables.txt, this will be dynamic soonish */ //decapricated for now, using a differnt method to get distance thanks to wiz!
-var mode = 0; //0 is default, 1 is testing (do not use testing for now)
+/* var enemydist = parent.G.monsters.<targettype>.range + 5; //Change <targettype> to whatever you are fighting, list of names for each monster will be in targettype variables.txt, this will be dynamic soonish */
+//deprecated for now, using a different method to get distance thanks to wiz!
+var mode = 0; //0 is kite(moveing around while attacking), 1 is standing still, 2 is testing (do not use testing for now)
 var min_xp_from_mob = 100; //set to minimum xp you want to be getting from each kill -- lowest amount of xp a mob has to have to be targetted
 var max_att_from_mob = 120; //set to maximum damage you want to take from each hit -- most attack you're willing to fight
 var min_xp_from_mob2 = 4000; //set to minimum xp you want to be getting from each kill if can't find min from first target -- lowest amount of xp a mob has to have to be targetted
@@ -15,158 +16,147 @@ var purchase_pots = true; //Set to true in order to allow potion purchases
 var pots_minimum = 50; //If you have less than this, you will buy
 var pots_to_buy = 1000; //This is how many you will buy
 
-setInterval(function()
-{
+setInterval(function() {
 
-    if (purchase_pots)
-    {
-        purchase_potions();
-    }
+	if (purchase_pots) {
+		purchase_potions();
+	}
 
-    if (character.hp / character.max_hp < 0.3)
-    {
-        parent.use('hp');
-        if (character.hp <= 100)
-            parent.socket.emit("transport",
-            {
-                to: "main"
-            });
-        //Panic Button
-    }
+	if (character.hp / character.max_hp < 0.3) {
+		parent.use('hp');
+		if (character.hp <= 100)
+			parent.socket.emit("transport", {
+				to: "main"
+			});
+		//Panic Button
+	}
 
-    if (character.mp / character.max_mp < 0.3)
-        parent.use('mp');
-    //Constrained Healing
+	if (character.mp / character.max_mp < 0.3)
+		parent.use('mp');
+	//Constrained Healing
 
-    var target = get_targeted_monster();
-    if (!target)
-    {
-        target = get_nearest_monster(
-        {
-            min_xp: min_xp_from_mob,
-            max_att: max_att_from_mob
-        });
-        if (target)
-            change_target(target);
-        else
-        {
-            target = get_nearest_monster(
-            {
-                min_xp: min_xp_from_mob2,
-                max_att: max_att_from_mob2
-            });
-            if (target)
-                change_target(target);
-            else
-            {
-                set_message("No Monsters");
-                return;
-            }
-        }
-    }
-    //Monster Searching
+	var target = get_targeted_monster();
+	if (!target) {
+		target = get_nearest_monster({
+			min_xp: min_xp_from_mob,
+			max_att: max_att_from_mob
+		});
+		if (target)
+			change_target(target);
+		else {
+			target = get_nearest_monster({
+				min_xp: min_xp_from_mob2,
+				max_att: max_att_from_mob2
+			});
+			if (target)
+				change_target(target);
+			else {
+				set_message("No Monsters");
+				return;
+			}
+		}
+	}
+	//Monster Searching
 
-    loot();
-    //Loot Chests
+	loot();
+	//Loot Chests
 
-    if (can_attack(target))
-        attack(target);
-    //Attack
+	if (can_attack(target))
+		attack(target);
+	//Attack
 
-    var parmem = get_nearest_player();
-    parent.socket.emit("party_invite",
-    {
-        id: parmem.id
-    });
-    //Invite to Party
+	var parmem = get_nearest_player();
+	parent.socket.emit("party_invite", {
+		id: parmem.id
+	});
+	//Invite to Party
 
-    var charx = character.real_x;
-    var chary = character.real_y;
-    //Character Location
+	var charx = character.real_x;
+	var chary = character.real_y;
+	//Character Location
 
-    var distx = target.real_x - charx;
-    var disty = target.real_y - chary;
-    //Enemy Distance
+	var distx = target.real_x - charx;
+	var disty = target.real_y - chary;
+	//Enemy Distance
 
-    set_message("Location:\n" +
-        Math.ceil(charx) + "," + Math.ceil(chary) +
-        "\nEnemy:\n" +
-        Math.ceil(target.real_x) + "," + Math.ceil(target.real_y) +
-        "\nDistance:\n" +
-        Math.ceil(distx) + "," + Math.ceil(disty));
-    //Output
+	set_message("Location:\n" +
+		Math.ceil(charx) + "," + Math.ceil(chary) +
+		"\nEnemy:\n" +
+		Math.ceil(target.real_x) + "," + Math.ceil(target.real_y) +
+		"\nDistance:\n" +
+		Math.ceil(distx) + "," + Math.ceil(disty));
+	//Output
 
-    if (mode === 0)
-    {
-        if (distx > 0) //Player is left of enemy
-        {
-            move(target.real_x - parent.G.monsters[target.mtype].range+5, chary);
-        }
-        if (distx < 0) //Player is right of enemy
-        {
-            move(target.real_x + parent.G.monsters[target.mtype].range+5, chary);
-        }
-        if (disty > 0) //Player is below enemy
-        {
-            move(charx, target.real_y - parent.G.monsters[target.mtype].range+5);
-        }
-        if (disty < 0) //Player is above enemy
-        {
-            move(charx, target.real_y + parent.G.monsters[target.mtype].range+5);
-        }
-    }
-/*    else if (mode == 1)
-    {
-        var tox, toy;
-        for (tox = -parent.G.monsters[target.mtype].range+5; tox <= parent.G.monsters[target.mtype].range+5; tox++)
-        {
-            for (toy = -parent.G.monsters[target.mtype].range+5; toy <= parent.G.monsters[target.mtype].range+5; toy++)
-            {
-                if (Math.pow(tox, 2) + Math.pow(toy, 2) == Math.pow(parent.G.monsters[target.mtype].range+5, 2))
-                    move(target.real_x + tox, target.real_y + toy);
-            }
-        }
-    } */
-    //Following/Maintaining Distance - Too Simplistic
+	if (mode === 0) {
+		if (distx > 0) //Player is left of enemy
+		{
+			move(target.real_x - parent.G.monsters[target.mtype].range + 5, chary);
+		}
+		if (distx < 0) //Player is right of enemy
+		{
+			move(target.real_x + parent.G.monsters[target.mtype].range + 5, chary);
+		}
+		if (disty > 0) //Player is below enemy
+		{
+			move(charx, target.real_y - parent.G.monsters[target.mtype].range + 5);
+		}
+		if (disty < 0) //Player is above enemy
+		{
+			move(charx, target.real_y + parent.G.monsters[target.mtype].range + 5);
+		}
+	} else if (mode == 1)
+		if (!in_attack_range(target)) {
+			move(
+				character.real_x + (target.real_x - character.real_x) / 2,
+				character.real_y + (target.real_y - character.real_y) / 2
+			);
+			// Walk half the distance
+		}
+		/*    else if (mode == 2)
+		    {
+		        var tox, toy;
+		        for (tox = -parent.G.monsters[target.mtype].range+5; tox <= parent.G.monsters[target.mtype].range+5; tox++)
+		        {
+		            for (toy = -parent.G.monsters[target.mtype].range+5; toy <= parent.G.monsters[target.mtype].range+5; toy++)
+		            {
+		                if (Math.pow(tox, 2) + Math.pow(toy, 2) == Math.pow(parent.G.monsters[target.mtype].range+5, 2))
+		                    move(target.real_x + tox, target.real_y + toy);
+		            }
+		        }
+		    } */
+		//Following/Maintaining Distance - Too Simplistic
 
-
-    prevx = Math.ceil(charx);
-    prevy = Math.ceil(chary);
-    //Sets new coords to prev coords
+	prevx = Math.ceil(charx);
+	prevy = Math.ceil(chary);
+	//Sets new coords to prev coords
 
 }, 200); // Loop Delay
 
-function isBetween(num, compare, range)
-{
-    return num >= compare - range && num <= compare + range;
+function isBetween(num, compare, range) {
+	return num >= compare - range && num <= compare + range;
 }
 
-function get_nearest_player()
-{
-    var min_d = 999999,
-        target = null;
-    for (var id in parent.entities)
-    {
-        var current = parent.entities[id];
-        if (current.type != "character" || current.dead)
-            continue;
-        var c_dist = parent.distance(character, current);
-        if (c_dist < min_d) min_d = c_dist, target = current;
-    }
-    return target;
+function get_nearest_player() {
+	var min_d = 999999,
+		target = null;
+	for (var id in parent.entities) {
+		var current = parent.entities[id];
+		if (current.type != "character" || current.dead)
+			continue;
+		var c_dist = parent.distance(character, current);
+		if (c_dist < min_d) min_d = c_dist, target = current;
+	}
+	return target;
 }
 
-function purchase_potions()
-{
-    set_message("Buying pots.");
-    if (character.items[0].q < pots_minimum)
-    {
-        parent.buy("hpot0", pots_to_buy);
-    }
-    if (character.items[1].q < pots_minimum)
-    {
-        parent.buy("mpot0", pots_to_buy);
-    }
+function purchase_potions() {
+	set_message("Buying pots.");
+	if (character.items[0].q < pots_minimum) {
+		parent.buy("hpot0", pots_to_buy);
+	}
+	if (character.items[1].q < pots_minimum) {
+		parent.buy("mpot0", pots_to_buy);
+	}
 }
 
 //Unusable:
--- Code Snippits.js
+++ Code Snippits.js
@@ -1,114 +1,100 @@
 //Returns the nearest living player.
-function get_nearest_player()
-{
-    var min_d = 999999,
-        target = null;
-    for (id in parent.entities)
-    {
-        var current = parent.entities[id];
-        if (current.player === false || current.dead) continue;
-        var c_dist = parent.distance(character, current);
-        if (c_dist < min_d) min_d = c_dist, target = current;
-        else if (current.player === true)
-        {
-            target = current;
-        }
-    }
-    return target;
+function get_nearest_player() {
+	var min_d = 999999,
+		target = null;
+	for (id in parent.entities) {
+		var current = parent.entities[id];
+		if (current.player === false || current.dead) continue;
+		var c_dist = parent.distance(character, current);
+		if (c_dist < min_d) min_d = c_dist, target = current;
+		else if (current.player === true) {
+			target = current;
+		}
+	}
+	return target;
 }
 
 //Returns the nearest living player not currently in a party
-function get_nearest_solo_player()
-{
-    var min_d = 999999,
-        target = null;
-    for (id in parent.entities)
-    {
-        var current = parent.entities[id];
-        if (current.player === false || current.dead || current.party) continue;
-        var c_dist = parent.distance(character, current);
-        if (c_dist < min_d) min_d = c_dist, target = current;
-        else if (current.player === true)
-        {
-            target = current;
-        }
-    }
-    return target;
+function get_nearest_solo_player() {
+	var min_d = 999999,
+		target = null;
+	for (id in parent.entities) {
+		var current = parent.entities[id];
+		if (current.player === false || current.dead || current.party) continue;
+		var c_dist = parent.distance(character, current);
+		if (c_dist < min_d) min_d = c_dist, target = current;
+		else if (current.player === true) {
+			target = current;
+		}
+	}
+	return target;
 }
 
 //Gets the nearest living member of your party
 //Modified by: Sulsaries
-function get_nearest_party_member()
-{
-    var min_d = 999999,
-        target = null;
-    for (id in parent.entities)
-    {
-        var current = parent.entities[id];
-        if (current.player === false || current.dead || current.party != character.party)
-        {
-            continue;
-        }
-        var c_dist = parent.distance(character, current);
-        if (c_dist < min_d) min_d = c_dist, target = current;
-        else if (current.player === true)
-        {
-            target = current;
-        }
-    }
-    return target;
+function get_nearest_party_member() {
+	var min_d = 999999,
+		target = null;
+	for (id in parent.entities) {
+		var current = parent.entities[id];
+		if (current.player === false || current.dead || current.party != character.party) {
+			continue;
+		}
+		var c_dist = parent.distance(character, current);
+		if (c_dist < min_d) min_d = c_dist, target = current;
+		else if (current.player === true) {
+			target = current;
+		}
+	}
+	return target;
 }
 
 //Fills the party_list with the living members of your party in the area.
 //Modified by: Sulsaries
 var party_count = 0;
 var party_list = [
-{
-    name: "",
-    priority: 0
+	{
+		name: "",
+		priority: 0
 },
-{
-    name: "",
-    priority: 0
+	{
+		name: "",
+		priority: 0
 },
-{
-    name: "",
-    priority: 0
+	{
+		name: "",
+		priority: 0
 },
-{
-    name: "",
-    priority: 0
+	{
+		name: "",
+		priority: 0
 },
-{
-    name: "",
-    priority: 0
+	{
+		name: "",
+		priority: 0
 },
-{
-    name: "",
-    priority: 0
+	{
+		name: "",
+		priority: 0
 }, ]
 
-function fill_party_list()
-{
-    var min_d = 999999,
-        target = null;
-    party_count = 0;
-    for (id in parent.entities)
-    {
-        var current = parent.entities[id];
-        if (current.player === false || current.dead || current.party != character.party)
-        {
-            continue;
-        }
-        var c_dist = parent.distance(character, current);
-        if (c_dist < min_d) min_d = c_dist, target = current;
-        else if (current.player === true)
-        {
-            target = current;
-            party_list[party_count].name = target.name;
-        }
-    }
-    return;
+function fill_party_list() {
+	var min_d = 999999,
+		target = null;
+	party_count = 0;
+	for (id in parent.entities) {
+		var current = parent.entities[id];
+		if (current.player === false || current.dead || current.party != character.party) {
+			continue;
+		}
+		var c_dist = parent.distance(character, current);
+		if (c_dist < min_d) min_d = c_dist, target = current;
+		else if (current.player === true) {
+			target = current;
+			party_list[party_count].name = target.name;
+		}
+	}
+	return;
 }
 
 // Auto Upgrade
@@ -117,37 +103,48 @@ var upgradeNumber = 0;
 var itemNumber = 0; //Change this zero to where your items to upgrade start
 
 //Inside setInterval
-if (upgradeNumber < 3 && itemNumber <= /last item to be upgraded/)
-{
-    upgrade(itemNumber, 39);
-    upgradeNumber++;
-    if (upgradeNumber == 3)
-    {
-        upgradeNumber = 0;
-        itemNumber++;
-    }
+if (upgradeNumber < 3 && itemNumber <= /last item to be upgraded/) {
+	upgrade(itemNumber, 39);
+	upgradeNumber++;
+	if (upgradeNumber == 3) {
+		upgradeNumber = 0;
+		itemNumber++;
+	}
 }
 
+//Auto Upgrade 2
+var item_slot = 0;
+var scroll_slot = 1;
+// Put the item to upgrade in slot 0, the scroll to use in slot 1
+// If the items are not present (used/destroyed), buy new ones.
+if (!character.items[item_slot])
+	parent.buy("gloves", 1);
+if (!character.items[scroll_slot])
+	parent.buy("scroll0", 200);
+
+upgrade(item_slot, scroll_slot)
+	// upgrade changes u_item and u_scroll, so I just change them directly.
+parent.u_item = item_slot;
+parent.u_scroll = scroll_slot;
+parent.upgrade();
+
 //Refined Potion Use
-if (character.hp <= character.max_hp - 200 || character.mp < character.mp_cost)
-{
-    use_hp_or_mp();
+if (character.hp <= character.max_hp - 200 || character.mp < character.mp_cost) {
+	use_hp_or_mp();
 }
 
 //refined Potion Use 2
-if (character.hp / character.max_hp < 0.3)
-{
-    parent.use('hp');
-    if (character.hp <= 100)
-        parent.socket.emit("transport",
-        {
-            to: "main"
-        });
-    //Panic Button
+if (character.hp / character.max_hp < 0.3) {
+	parent.use('hp');
+	if (character.hp <= 100)
+		parent.socket.emit("transport", {
+			to: "main"
+		});
+	//Panic Button
 }
 
 if (character.mp / character.max_mp < 0.3)
-    parent.use('mp');
+	parent.use('mp');
 //Constrained Healing
 
 //Automatic Potion Purchasing!
@@ -156,23 +153,19 @@ var purchase_pots = true; //Set to true in order to allow potion purchases
 var pots_minimum = 50; //If you have less than this, you will buy
 var pots_to_buy = 100; //This is how many you will buy
 
-function purchase_potions()
-{
-    set_message("Buying pots.");
-    if (character.items[0].q < pots_minimum)
-    {
-        parent.buy("hpot0", pots_to_buy);
-    }
-    if (character.items[1].q < pots_minimum)
-    {
-        parent.buy("mpot0", pots_to_buy);
-    }
+function purchase_potions() {
+	set_message("Buying pots.");
+	if (character.items[0].q < pots_minimum) {
+		parent.buy("hpot0", pots_to_buy);
+	}
+	if (character.items[1].q < pots_minimum) {
+		parent.buy("mpot0", pots_to_buy);
+	}
 }
 
 //Place inside setInterval to check potions when turned on.
-if (purchase_pots)
-{
-    purchase_potions();
+if (purchase_pots) {
+	purchase_potions();
 }
 
 //Checks to see if your character has moved.
@@ -182,16 +175,14 @@ var deltaY = 0;
 var oldX = character.real_x;
 var oldY = character.real_y;
 
-function has_moved()
-{
-    deltaX = character.real_x - oldX;
-    deltaY = character.real_y - oldY;
-    var moved = false;
-    if (deltaX || deltaY)
-    {
-        moved = true;
-    }
-    oldX = character.real_x;
-    oldY = character.real_y;
-    return moved;
+function has_moved() {
+	deltaX = character.real_x - oldX;
+	deltaY = character.real_y - oldY;
+	var moved = false;
+	if (deltaX || deltaY) {
+		moved = true;
+	}
+	oldX = character.real_x;
+	oldY = character.real_y;
+	return moved;
 }
