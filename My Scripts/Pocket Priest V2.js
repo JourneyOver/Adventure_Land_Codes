3015ee85935e6f7a9efdf67a4a6e963a3a795e8d
JourneyOver <timtag1190@gmail.com>
2016-11-03 11:59:27 -0500

    Update Pocket Priest
    ~Script now waits until party leader has tagged the target before cursing/attacking.

--- My Scripts/Pocket Priest V2.js
+++ My Scripts/Pocket Priest V2.js
@@ -3,7 +3,7 @@
 // It looks at their max hp vs current hp and heals the person with the highest percentage loss.
 // Courtesy of: Mark
 // Edits & Additions By: JourneyOver
-// Version 1.1.0
+// Version 1.1.1
 
 var till_level = 0; //Kills till level = 0, XP till level = 1
 var heal_dist = 0; //Stay at a distance and move when out of range of target/leader (only when leader is attacking something) = 0, Stay always on top of leader = 1
@@ -46,17 +46,18 @@ setInterval(function() {
     purchase_potions(buy_hp, buy_mp);
   }
 
+  //Get the Party leader
   let leader = get_player(character.party);
 
-  // Get the injured party members.
+  //Get the injured party members.
   let injured = GetInjured(leader.name);
 
-  // Heal a party member
+  //Heal a party member
   if (injured.length > 0) {
     let target = injured[0];
 
     for (let i = 1; i < injured.length; i++) {
-      // Target the party member with the lowest amount of hp
+      //Target the party member with the lowest amount of hp
       if (injured[i].max_hp - injured[i].hp > target.max_hp - target.hp)
         target = injured[i];
     }
@@ -64,33 +65,33 @@ setInterval(function() {
     heal(target);
     set_message("Healing " + target.name);
   }
-  // Do damage.
+  //Do damage.
   else {
-    // Get the target of the leader.
+    //Get the target of the leader.
     change_target(get_target_of(leader));
     target = get_target();
 
-    // If there is a valid target, attempt to curse it.
-    if (target && in_attack_range(target)) {
+    //If there is a valid target, attempt to curse it.
+    if (target && in_attack_range(target) && get_target_of(target).party == character.party) {
       curse(target);
-      set_message("I'm Cursing " + target.mtype);
+      set_message("Cursing " + target.mtype);
 
-      // If you can attack the target, do so.
+      //If you can attack the target, do so.
       if (can_attack(target))
         attack(target);
-      set_message("I'm Attacking " + target.mtype);
+      set_message("Attacking " + target.mtype);
     }
   }
 
   //Move when out of range of target/leader (only when leader is attacking)
   if (heal_dist === 0)
     if (!in_attack_range(target))
-    // Move only if you are not already moving.
+    //Move only if you are not already moving.
       move_to(target, character.range);
   if (heal_dist === 1)
   //Stay ontop of leader.
     if (!character.moving)
-    // Move only if you are not already moving.
+    //Move only if you are not already moving.
       move(leader.real_x, leader.real_y);
 
 }, 1000 / 4);
@@ -117,7 +118,7 @@ function move_to(char, distance) {
 var lastcurse;
 
 function curse(target) {
-  // Curse only if target hasn't been cursed and if curse off cd (cd is 5sec).
+  //Curse only if target hasn't been cursed and if curse off cd (cd is 5sec).
   if ((!lastcurse || new Date() - lastcurse > 5000) && !target.cursed) {
     lastcurse = new Date();
     parent.socket.emit("ability", {
@@ -127,11 +128,11 @@ function curse(target) {
   }
 }
 
-// Returns the injured party members.
+//Returns the injured party members.
 function GetInjured(leader) {
-  // List of party members.
+  //List of party members.
   let res = [];
-  // Only heal targets below 80% hp.
+  //Only heal targets below 80% hp.
   let percentage = 0.8;
 
   for (id in parent.entities) {
