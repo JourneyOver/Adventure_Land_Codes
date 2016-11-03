de937be88e8df0c9737fb29386703c61c5bdb9eb
JourneyOver <timtag1190@gmail.com>
2016-11-02 23:06:48 -0500

    Update Scripts!
    ~Change method of finding potions in inventory so no more overbuying potions
    ~Added a couple more variables to auto potion buying

--- My Scripts/Pocket Priest V2.js
+++ My Scripts/Pocket Priest V2.js
@@ -3,13 +3,17 @@
 // It looks at their max hp vs current hp and heals the person with the highest percentage loss.
 // Courtesy of: Mark
 // Edits & Additions By: JourneyOver
-// Version 1.0.0
+// Version 1.1.0
 
 var till_level = 0; //Kills till level = 0, XP till level = 1
 var heal_dist = 0; //Stay at a distance and move when out of range of target/leader (only when leader is attacking something) = 0, Stay always on top of leader = 1
 //Main Settings
 
 var purchase_pots = false; //Set to true in order to allow potion purchases
+var buy_hp = false; //Set to true in order to allow hp potion purchases
+var buy_mp = false; //Set to true in order to allow mp potion purchases
+var hp_potion = 'hpot0'; //+200 HP Potion = 'hpot0', +400 HP Potion = 'hpot1' [always keep '' around it]
+var mp_potion = 'mpot0'; //+300 MP Potion = 'mpot0', +500 MP Potion = 'mpot1' [always keep '' around it]
 var pots_minimum = 50; //If you have less than this, you will buy
 var pots_to_buy = 1000; //This is how many you will buy
 //Automatic Potion Purchasing!
@@ -39,7 +43,7 @@ setInterval(function() {
 
   //Purchases Potions when below threshold
   if (purchase_pots) {
-    purchase_potions();
+    purchase_potions(buy_hp, buy_mp);
   }
 
   let leader = get_player(character.party);
@@ -150,14 +154,29 @@ function GetInjured(leader) {
 }
 
 //Purchase Potions
-function purchase_potions() {
-  if (character.items[0].q < pots_minimum) {
-    parent.buy("hpot0", pots_to_buy);
+function purchase_potions(buyHP, buyMP) {
+  let [hpslot, hppot] = find_item(i => i.name == hp_potion);
+  let [mpslot, mppot] = find_item(i => i.name == mp_potion);
+
+  if (buyHP && (!hppot || hppot.q < pots_minimum)) {
+    parent.buy(hp_potion, pots_to_buy);
   }
-  if (character.items[1].q < pots_minimum) {
-    parent.buy("mpot0", pots_to_buy);
-    set_message("Buying pots.");
+  if (buyMP && (!mppot || mppot.q < pots_minimum)) {
+    parent.buy(mp_potion, pots_to_buy);
   }
+
+  set_message("Buying pots.");
+}
+
+function find_item(filter) {
+  for (let i = 0; i < character.items.length; i++) {
+    let item = character.items[i];
+
+    if (item && filter(item))
+      return [i, character.items[i]];
+  }
+
+  return [-1, null];
 }
 
 //XP/Kill Till level + Gold GUI
