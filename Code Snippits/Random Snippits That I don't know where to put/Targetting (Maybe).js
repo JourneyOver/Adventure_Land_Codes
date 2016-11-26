8e4f0e3255119e823e79a49d75f5fc33a4dc6b32
Journey <timtag1190@gmail.com>
2016-11-26 05:32:19 -0600

    adding this for future use?

--- Code Snippits/Random Snippits That I don't know where to put/Targetting (Maybe).js
+++ Code Snippits/Random Snippits That I don't know where to put/Targetting (Maybe).js
@@ -1,12 +1,12 @@
     // BEGIN TARGETING CODE
     let all_enemies = Object.values(parent.entities).filter(entity => !parent.party_list.includes(entity.name));
     if (!parent.G.maps[character.map].pvp || character.goldm != 1) { all_enemies = filter_monster_only(all_enemies); }
-    
+
     // SCORE ENEMIES
     let enemy_scores = all_enemies.map(m => {
         // SHORT CIRCUIT PARTY MEMBERS
         if (m.name && parent.party_list.includes(m.name))           { return [0, null];  }
-        
+
         let score = 0;
         // TARGET SCORING
         if ((parent.G.maps[character.map].pvp || character.goldm != 1)
---
+++ Code Snippits/Random Snippits That I don't know where to put/more path finding stuff (I believe).js
@@ -0,0 +1,45 @@
+let map_graph = initialize_graph(parent.current_map);
+let cur_node = map_graph.get(character.real_x, character.real_y);
+let target_node = map_graph.get(0, 0);
+let path = find_path(cur_node, target_node);
+console.info(path);
+
+/////////////////////////////////////////////////
+
+const graph_cache = {};
+export async function go_to_point(point) {
+    if (!graph_cache[point.map]) {
+        console.time('Initialize Graph');
+        graph_cache[point.map] = initialize_graph(point.map);
+        console.timeEnd('Initialize Graph');
+    }
+
+    let map = graph_cache[point.map];
+
+    let current_node = map.get(character.real_x, character.real_y);
+    let target_node = map.get(point.x, point.y);
+
+    let current_virtual = new VirtualNode(current_node, character.real_x, character.real_y);
+    let target_virtual = new VirtualNode(target_node, point.x, point.y);
+
+    console.time('Find path');
+    let path = find_path(current_virtual, target_virtual);
+    console.timeEnd('Find path');
+
+    current_virtual.destroy();
+    target_virtual.destroy();
+
+    while (path.length) {
+        let target = path.shift();
+        move(target.x, target.y);
+
+        while (character.moving) {
+            await sleep(1000 / 20);
+        }
+
+        let pos = new Vec(character);
+        // Unexpected movement (probably by the player), so cancel
+        // the path movement.
+        if (!pos.equals(target)) break;
+    }
+}
